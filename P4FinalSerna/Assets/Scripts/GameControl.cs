042d996dd4d439712322814877cfd82cdcfc6ff3
AARONS-1 <751727@student.dusd.net>
2024-12-11 11:45:05 -0800

    added components
    I added  some code to my game and fixed  the words on screen

--- P4FinalSerna/Assets/Scripts/GameControl.cs
+++ P4FinalSerna/Assets/Scripts/GameControl.cs
@@ -3,26 +3,39 @@ using System.Collections.Generic;
 using JetBrains.Annotations;
 using UnityEditor.Experimental.GraphView;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameControl : MonoBehaviour
 {
     public static GameControl instance;
     public GameObject gameOverText;
     public bool gameOver = false;
+    public float scrollSpeed = -1.5f;
 
     // Start is called before the first frame update
-    void Awake ()
+    void Awake()
     {
-        
-        
+        if (instance == null)
+        {
+            instance = this;
+        }
+        else if (instance != this)
+        {
+            Destroy(gameObject);
+        }
     }
     
         
 
     // Update is called once per frame
-    void Update() {
-
+    void Update()
+    {
+        if (gameOver == true && Input.GetMouseButtonDown (0))
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        }
     }
+    
         
     public void BirdDied()
     {
